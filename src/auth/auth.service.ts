2c55de63342998e3eb82ead07caabb96234416bb
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-07-09 08:25:57 -0600

    strict DTO

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
+
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 
+import { CreateUserDto } from './dto/create-user.dto';
+
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
+  create(createUserDto: CreateUserDto) {
+    console.log(createUserDto);
     return 'This action adds a new auth';
   }
 
